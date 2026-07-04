529efd98a51c4ca6f3caf0b6fdb74df15944accb
BackendIsFishy <himanshumahor72@gmail.com>
2026-07-04 16:12:08 +0530

    first_upload

---
+++ JavaScript/script.js
@@ -0,0 +1,23 @@
+document.querySelector('#add-task').onclick = function(){
+    if(document.querySelector('#task').value.length==0){
+        alert('Please enter a task');
+    }
+    else{
+        document.querySelector('#main-div2').innerHTML += `
+        <div>
+        <p class='text-left' style='display:inline-block ;min-width:80%' onclick="check(this)">${document.querySelector('#task').value}</p><span class='btn btn-danger btn-sm align-top float-end' style='display:inline-block;' onclick="this.parentElement.remove()">Delete</span>
+        <br>
+        </div>
+        `
+        document.querySelector('#task').value = '';
+    }
+}
+
+function check(element){
+    if(element.style.textDecoration == 'line-through'){
+        element.style.textDecoration = 'none';
+    }
+    else{
+        element.style.textDecoration = 'line-through';
+    }
+}
